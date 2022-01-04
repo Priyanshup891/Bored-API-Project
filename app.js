1c9e0423e7f0fed86da7e4716c81938ddae554e3
Priyanshu Patil <80090818+Priyanshup891@users.noreply.github.com>
2022-01-04 13:01:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const find_btn = document.querySelector('.btn');
 const activity = document.querySelector('.activity');
-const enter_parti = document.querySelector('.enter-parti');
 
 find_btn.addEventListener('click', async () => {
     try{
-        const participate = enter_parti.value;
-        const URL = `https://www.boredapi.com/api/activity?participants=${participate}`
+        const URL = `https://www.boredapi.com/api/activity?participants=1`;
         const data = await fetch(URL);
         const responce = await data.json();
         activity.textContent = responce.activity;
-
-        if (participate == ""){
-            activity.textContent = "Please, enter the participants";
-        }
     } catch(error){
         console.log("error !", error);
         activity.textContent = "Something's Wrong !";
